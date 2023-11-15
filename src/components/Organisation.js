ae6f5603220a90cfa4232e0a37f0e8711b9bfb0a
kallolghosal <kghosal@gmail.com>
2023-11-15 11:15:49 +0530

    Updated version

--- src/components/Organisation.js
+++ src/components/Organisation.js
@@ -34,9 +34,9 @@ export class Organisationsec extends Component {
                             <div className="organisation-logo-div-1"><img src={vrutilogo} alt="Vruti Logo" width={170} /></div>
                             <div className="organisation-details">
                                 <p>A registered Society (not-for-profit), Vrutti is a Livelihood Impact Partner which enhances people&apos;s well-being through knowledge, innovation and transformative actions and brings about solutions at a scale that eliminate inequities and create wealth for the </p>
-                                <p><a href="https://vruttiimpactcatalysts.org/" target="blank" rel="noreferrer"> Visit Website </a></p>
+                                <p><a href="https://vruttiimpactcatalysts.org/" target="blank" rel="noopener noreferrer"> Visit Website </a></p>
                             </div>
-                            <a href="https://vruttiimpactcatalysts.org/" target="_blank" rel="noreferrer" className="organisation-readmore">READ MORE</a>
+                            <a href="https://vruttiimpactcatalysts.org/" target="_blank" rel="noopener noreferrer" className="organisation-readmore">READ MORE</a>
                         </div>
                         <div className="organisation-single wow fadeIn slick-slide slick-cloned">
                             <div className="organisation-logo-div-1"><img src={greenfnd} alt="Green Foundation Logo" width={170} /></div>
@@ -44,7 +44,7 @@ export class Organisationsec extends Component {
                                 <p>GREEN Foundation works toward a well-preserved, diverse ecosystem that will sustain the rural livelihoods of the present generation without eroding the resource base of the future. Our work to empower the small-scale and marginal farmers of India first began in 1994 with</p>
                                 <p><a className="directions" href="https://greenfoundation.in/" target="blank" rel="noopener noreferrer"> Visit Website </a></p>
                             </div>
-                            <a href="https://www.greenfoundation.in/" target="_blank" rel="noreferrer" className="organisation-readmore">READ MORE</a>
+                            <a href="https://www.greenfoundation.in/" target="_blank" rel="noopener noreferrer" className="organisation-readmore">READ MORE</a>
                         </div>
                         <div className="organisation-single wow fadeIn slick-slide slick-cloned">
                             <div className="organisation-logo-div-1"><img src={fughiologo} alt="Fuzhio Logo" width={170} /></div>
@@ -52,7 +52,7 @@ export class Organisationsec extends Component {
                                 <p>Facilitating Market Access to Women and Smallholder Farmers and Marginalised Communities to Maximise Returns for their Products and Services. Fuzhio promotes Impact Products directly to customers on behalf of small farmers and vulnerable groups and </p>
                                 <p><a className="directions" href="https://fuzhio.org/" target="blank" rel="noopener noreferrer"> Visit Website </a></p>
                             </div>
-                            <a href="https://fuzhio.org/" target="_blank" rel="noreferrer" className="organisation-readmore">READ MORE</a>
+                            <a href="https://fuzhio.org/" target="_blank" rel="noopener noreferrer" className="organisation-readmore">READ MORE</a>
                         </div>
                         <div className="organisation-single wow fadeIn slick-slide slick-cloned">
                             <div className="organisation-logo-div-1"><img src={upfront} alt="Upfront Logo" width={170} /></div>
@@ -60,7 +60,7 @@ export class Organisationsec extends Component {
                                 <p>Upfront works with front line workers and the “systems” by changing mindsets, behaviours and practices; improving skills, financial security, processes and services; building agency, ensuring voices and choices; and establishing partnerships and collaborations.</p>
                                 <p><a className="directions" href="https://upfront.global/" target="blank" rel="noopener noreferrer"> Visit Website </a></p>
                             </div>
-                            <a href="https://upfront.global/" target="_blank" rel="noreferrer" className="organisation-readmore">READ MORE</a>
+                            <a href="https://upfront.global/" target="_blank" rel="noopener noreferrer" className="organisation-readmore">READ MORE</a>
                         </div>
                         <div className="organisation-single wow fadeIn slick-slide slick-cloned">
                             <div className="organisation-logo-div-1"><img src={swasti} alt="Swasti Logo" width={170} /></div>
@@ -68,7 +68,7 @@ export class Organisationsec extends Component {
                                 <p>A registered Society (not-for-profit), Swasti &mdash; The Health Catalyst &mdash; is a health resource centre.Which achieves public health outcomes for the marginalised, through the delivery of end-to-end solutions as well as short and long term support and facilitation,</p>
                                 <p><a href="https://swastihc.org" className="directions" target="blank" rel="noopener noreferrer"> Visit Website </a></p>
                             </div>
-                            <a href="https://swastihc.org/" target="_blank" rel="noreferrer" className="organisation-readmore">READ MORE</a>
+                            <a href="https://swastihc.org/" target="_blank" rel="noopener noreferrer" className="organisation-readmore">READ MORE</a>
                         </div>
                         <div className="organisation-single wow fadeIn slick-slide slick-cloned">
                             <div className="organisation-logo-div-1"><img src={cms} alt="CMS Logo" width={170} /></div>
@@ -76,7 +76,7 @@ export class Organisationsec extends Component {
                                 <p>A private limited organisation which specializes in providing consulting and professional services. CMS partners to create social impact, guaranteeing results through development solutions and investments. The organisation commits to sustainable development that </p>
                                 <p><a className="directions" href="https://solvists.org/" target="blank" rel="noopener noreferrer"> Visit Website </a></p>
                             </div>
-                            <a href="https://solvists.org/" target="_blank" rel="noreferrer" className="organisation-readmore">READ MORE</a>
+                            <a href="https://solvists.org/" target="_blank" rel="noopener noreferrer" className="organisation-readmore">READ MORE</a>
                         </div>
                     </Slider>
                 </Container>
