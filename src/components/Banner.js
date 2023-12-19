55957bb5d3cd080c660725cf4121e4a6768887b8
kallolghosal <kghosal@gmail.com>
2023-12-19 12:09:25 +0530

    Banner update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,22 +1,34 @@
 import React, { Component } from "react";
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Carousel } from 'react-bootstrap';
 import logo from '../img/catalyst-global-logo.svg';
 
 export class Banner extends Component {
     render () {
         return (
-            <Container fluid style={ { margin:0, padding:0 } }>
-                <div className="fp-hero">
-                    <div className="bx-wrapper"></div>
-                    <div className="fp-hero-center">
-                        <img src={logo} alt="Catalyst Global Logo" />
-                        <p>
-                        Catalyst Foundation is a social enterprise platform that seeks to promote health and wealth of vulnerable communities.
-                        </p>
-                    </div>
-                    <Button href="" className="hero-findbtn wow fadeInUp">FIND OUT MORE</Button>
-                </div>
-            </Container>
+            <Carousel>
+                <Carousel.Item>
+                    <Container fluid className="banner-bg-1">
+                        <div className="fp-hero">
+                            <div className="fp-hero-center">
+                                <img src={logo} alt="Catalyst Global Logo" />
+                                <p>Catalyst Foundation is a social enterprise platform that seeks to promote health and wealth of vulnerable communities.</p>
+                            </div>
+                            <Button href="" className="hero-findbtn wow fadeInUp">FIND OUT MORE</Button>
+                        </div>
+                    </Container>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Container fluid className="banner-bg-2">
+                        <div className="fp-hero">
+                            <div className="fp-hero-center">
+                                <img src={logo} alt="Catalyst Global Logo" />
+                                <p>Catalyst Foundation is a social enterprise platform that seeks to promote health and wealth of vulnerable communities.</p>
+                            </div>
+                            <Button href="" className="hero-findbtn wow fadeInUp">FIND OUT MORE</Button>
+                        </div>
+                    </Container>
+                </Carousel.Item>
+            </Carousel>
         )
     }
 }
